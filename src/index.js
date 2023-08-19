6c5ea18516b15c35e2b21e33350730bfeced56db
Umu <hanny.ally123@gmail.com>
2023-08-19 22:20:46 +0300

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,39 +1,84 @@
-// import _ from "lodash";
-import './style.css';
-// import Icon from './images/cheched.jpg';
+// lacal storage
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const tasks = [
-  { description: 'Task 1', completed: false, index: 1 },
-  { description: 'Task 2', completed: true, index: 2 },
-  { description: 'Task 3', completed: false, index: 3 },
-];
-
+// Function to render tasks on the page
 function renderTasks() {
-  const listElement = document.querySelector('.list');
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
 
-  const inputEl = document.createElement('input');
-  inputEl.classList.add('input');
-  inputEl.placeholder = 'Add your task here';
-  listElement.appendChild(inputEl);
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = `${index + 1}. ${task.description}`;
 
-  tasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = task.description;
-    listItem.classList.add(task.completed ? 'completed' : 'not-completed');
-    listElement.appendChild(listItem);
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.classList.add("btn");
+    editButton.addEventListener("click", () => editTask(index));
 
-    const span = document.createElement('div');
-    span.classList.add('close');
-    span.innerHTML = '⋮';
-    listItem.appendChild(span);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("btn");
+    deleteButton.addEventListener("click", () => deleteTask(index));
+
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
   });
-
-  const btn = document.createElement('button');
-  btn.textContent = 'Clear all completed';
-  btn.classList.add('btn');
-  listElement.appendChild(btn);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+// Function to add a new task
+function addTask(description) {
+  const newTask = {
+    description: description,
+    completed: false,
+    index: tasks.length + 1,
+  };
+
+  tasks.push(newTask);
+  updateIndexes();
+  saveTasksToLocalStorage();
   renderTasks();
+}
+
+// Function to delete a task
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  updateIndexes();
+  saveTasksToLocalStorage();
+  renderTasks();
+}
+
+// Function to edit a task's description
+function editTask(index) {
+  const newDescription = prompt("Enter a new description for the task:");
+  if (newDescription !== null) {
+    tasks[index].description = newDescription;
+    saveTasksToLocalStorage();
+    renderTasks();
+  }
+}
+
+// Function to update task indexes after delete
+function updateIndexes() {
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+}
+
+// Function to save tasks to local storage
+function saveTasksToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Add event listener for adding tasks
+const addTaskButton = document.getElementById("addTask");
+addTaskButton.addEventListener("click", () => {
+  const newTaskInput = document.getElementById("newTask");
+  const newTaskDescription = newTaskInput.value.trim();
+  if (newTaskDescription !== "") {
+    addTask(newTaskDescription);
+    newTaskInput.value = "";
+  }
 });
+
+renderTasks();
