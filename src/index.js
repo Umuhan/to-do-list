77997ae1a17ca8cd984712fdf7c27a6d914b49e8
Umu <hanny.ally123@gmail.com>
2023-08-22 17:40:28 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import "./style.css";
+import './style.css';
 // references to the input, button, and task container elements
-const taskInput = document.getElementById("taskInput");
-const addTaskButton = document.getElementById("addTaskButton");
-const taskContainer = document.getElementById("taskContainer");
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskContainer = document.getElementById('taskContainer');
 
 let tasks = [];
 
+// Function to save tasks to local storage
+function saveTasksToLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 // Function to update the task container
 function updateTaskContainer() {
-  taskContainer.innerHTML = "";
+  taskContainer.innerHTML = '';
 
   tasks.forEach((task, index) => {
-    const taskItem = document.createElement("ul");
-    taskItem.classList.add("task-item");
+    const taskItem = document.createElement('ul');
+    taskItem.classList.add('task-item');
 
-    const taskContent = document.createElement("li");
+    const taskContent = document.createElement('li');
     taskContent.textContent = task.description;
-    const editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.classList.add("btn");
-    editButton.addEventListener("click", () => {
-      const newDescription = prompt("Enter a new description for the task:");
-
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.classList.add('btn');
+    editButton.addEventListener('click', () => {
+      const newDescription = prompt('Enter a new description for the task:');
       if (newDescription !== null) {
         tasks[index].description = newDescription;
         updateTaskContainer();
@@ -29,10 +33,10 @@ function updateTaskContainer() {
       }
     });
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.classList.add("btn");
-    deleteButton.addEventListener("click", () => {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('btn');
+    deleteButton.addEventListener('click', () => {
       tasks.splice(index, 1);
       for (let i = 0; i < tasks.length; i += 1) {
         tasks[i].index = i + 1;
@@ -51,7 +55,7 @@ function updateTaskContainer() {
 // Function to add a new task
 function addTask(description) {
   const newTask = {
-    description: description,
+    description,
     completed: false,
     index: tasks.length + 1,
   };
@@ -61,16 +65,9 @@ function addTask(description) {
   saveTasksToLocalStorage();
 }
 
-function edditTask(index) {}
-
-// Function to save tasks to local storage
-function saveTasksToLocalStorage() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
 // Function to load tasks from local storage
 function loadTasksFromLocalStorage() {
-  const storedTasks = localStorage.getItem("tasks");
+  const storedTasks = localStorage.getItem('tasks');
   if (storedTasks) {
     tasks = JSON.parse(storedTasks);
     updateTaskContainer();
@@ -78,15 +75,15 @@ function loadTasksFromLocalStorage() {
 }
 
 //  event listener to the "Add Task" button
-addTaskButton.addEventListener("click", () => {
+addTaskButton.addEventListener('click', () => {
   const taskText = taskInput.value;
-  if (taskText !== "") {
+  if (taskText !== '') {
     addTask(taskText);
-    taskInput.value = "";
+    taskInput.value = '';
   }
 });
 
 // Loading tasks from local storage on page load
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   loadTasksFromLocalStorage();
 });
