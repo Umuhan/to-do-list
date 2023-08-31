6155ae2ee731a55390c45dfc88d7377c586d80ea
Umu <hanny.ally123@gmail.com>
2023-08-31 22:16:10 +0300

    Fix eslint

--- src/tests/addTask.test.js
+++ src/tests/addTask.test.js
@@ -1,13 +1,13 @@
-const addTask = require("./_mock/addTask.js");
+const addTask = require('./_mock/addTask.js');
 
-describe("Adding new tasks", () => {
-  test("addTask function exists", () => {
-    expect(typeof addTask).toEqual("function");
+describe('Adding new tasks', () => {
+  test('addTask function exists', () => {
+    expect(typeof addTask).toEqual('function');
   });
 
-  test("should add a task to the tasks array", () => {
+  test('should add a task to the tasks array', () => {
     const tasks = [];
-    const description = "Test task";
+    const description = 'Test task';
     addTask(description, tasks);
 
     expect(tasks).toHaveLength(1);
